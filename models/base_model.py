cb22b297b38d7ad86a35264316e8344718619ed9
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2024-08-03 00:23:34 +0300

    Task 4

--- models/base_model.py
+++ models/base_model.py
@@ -10,10 +10,28 @@ import json
 
 
 class BaseModel():
-    def __init__(self) -> None:
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs) -> None:
+        # some keyword arguments were passed.. I add them to the class
+        if (kwargs):
+            # Remove the __class__ attribute (if any)
+            kwargs.pop('__class__', None)
+
+            # Handle datetime strings
+            # add them as parsed objects and remove the strings
+            self.created_at = datetime.fromisoformat(kwargs['created_at'])
+            self.updated_at = datetime.fromisoformat(kwargs['updated_at'])
+            kwargs.pop('created_at', None)
+            kwargs.pop('updated_at', None)
+
+            # add the remaining attributes
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+        # empty argument list
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self) -> str:
         string = f'[BaseModel] ({self.id}) {self.__dict__}'
@@ -21,9 +39,9 @@ class BaseModel():
 
     def save(self) -> None:
         self.updated_at = datetime.now()
-        dict = self.to_dict()
-        with open('output.json', 'w') as json_file:
-            json.dump(dict, json_file, indent=4)
+        # dict = self.to_dict()
+        # with open('output.json', 'w') as json_file:
+        #     json.dump(dict, json_file, indent=4)
 
     def to_dict(self):
         dict = copy.deepcopy(self.__dict__)
