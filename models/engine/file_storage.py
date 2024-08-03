f075d46a9cf12f43e0566c46425dc868d4b2b316
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2024-08-04 00:30:28 +0300

    make files pycodestyle compliant

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,42 +6,42 @@ import models
 
 
 class FileStorage():
-	def __init__(self) -> None:
-		self.__file_path = './file.json'
-		self.__objects = {}
-		# __objects stores objects with keys like this: <class name>.id
+    def __init__(self) -> None:
+        self.__file_path = './file.json'
+        self.__objects = {}
+        # __objects stores objects with keys like this: <class name>.id
 
-	def all(self) -> dict:
-		'''returns the dictionary __objects'''
-		return self.__objects
-	
-	def new(self, obj):
-		'''sets in __objects the obj with key <obj class name>.id'''
-		self.__objects[f'{obj.__class__}.{obj.id}'] = obj
+    def all(self) -> dict:
+        '''returns the dictionary __objects'''
+        return self.__objects
 
-	def save(self):
-		'''serializes __objects to the JSON file (path: __file_path)'''
-		# convert the __objects values from objects to dictionaries
-		converted_objs = {
-      		key: obj.to_dict() for key, obj in self.__objects.items()
+    def new(self, obj):
+        '''sets in __objects the obj with key <obj class name>.id'''
+        self.__objects[f'{obj.__class__}.{obj.id}'] = obj
+
+    def save(self):
+        '''serializes __objects to the JSON file (path: __file_path)'''
+        # convert the __objects values from objects to dictionaries
+        converted_objs = {
+              key: obj.to_dict() for key, obj in self.__objects.items()
         }
-  
-		# json-dump the converted dictionary
-		with open(self.__file_path, 'w') as json_file:
-			json.dump(converted_objs, json_file, indent=4)
 
-	def reload(self):
-		'''deserializes the JSON file to __objects only if the JSON file
-		at __file_path exists. Otherwise, does nothing'''
-		# Stop the function is no such file exists
-		if not os.path.exists(self.__file_path):
-			return
+        # json-dump the converted dictionary
+        with open(self.__file_path, 'w') as json_file:
+            json.dump(converted_objs, json_file, indent=4)
 
-		with open(self.__file_path, 'r') as json_file:
-			all_obj_dicts = json.load(json_file)  # All objects dictionaries
-			# For each one, find the class, and create it
-			for key, obj_dict in all_obj_dicts.items():
-				class_name = obj_dict['__class__']
-				_class = models.class_registry.get(class_name, None)
-				if _class:
-					self.__objects[key] = _class(**obj_dict)
+    def reload(self):
+        '''deserializes the JSON file to __objects only if the JSON file
+        at __file_path exists. Otherwise, does nothing'''
+        # Stop the function is no such file exists
+        if not os.path.exists(self.__file_path):
+            return
+
+        with open(self.__file_path, 'r') as json_file:
+            all_obj_dicts = json.load(json_file)  # All objects dictionaries
+            # For each one, find the class, and create it
+            for key, obj_dict in all_obj_dicts.items():
+                class_name = obj_dict['__class__']
+                _class = models.class_registry.get(class_name, None)
+                if _class:
+                    self.__objects[key] = _class(**obj_dict)
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,25 +3,26 @@ import unittest
 from datetime import datetime
 from models.base_model import BaseModel
 
+
 class TestBaseModel(unittest.TestCase):
     def test_init(self):
         self.assertIsInstance(BaseModel(), BaseModel)
 
     def test_str(self):
         self.assertTrue(str(BaseModel()).startswith('[BaseModel]'))
-        
+
     def test_save(self):
         myObj = BaseModel()
         myObj.save()
         self.assertNotEqual(myObj.updated_at, datetime.now())
-        
+
     def test_to_dict(self):
         myObj = BaseModel()
         my_dict = myObj.to_dict()
         self.assertEqual(my_dict['__class__'], 'BaseModel')
         self.assertIsInstance(my_dict['created_at'], str)
         self.assertIsInstance(my_dict['updated_at'], str)
-    
+
 
 if __name__ == '__main__':
     unittest.main()
