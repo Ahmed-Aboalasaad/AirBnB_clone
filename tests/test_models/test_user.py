f1182bd51c89727a3710ededaab4dd142727868f
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2024-08-06 11:35:54 +0300

    make test files pycodestyle compliant

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,10 +2,12 @@
 from models.user import User
 import unittest
 
+
 class TestUser(unittest.TestCase):
     def test_user_email(self):
         self.assertIs(type(User.email), str)
         self.assertEqual(User.email, "")
+
 
 if __name__ == '__main__':
     unittest.main()
